fc5f6fba807297ba899ca072e070a6d7509269d0
Philipp Limmer <philipp.limmer@web.de>
2023-06-13 21:00:51 +0200

    [TASK] Basic ftp update system

--- src/main/java/com/limmerlaboratories/scriptrunner/controller/PostConstructController.java
+++ src/main/java/com/limmerlaboratories/scriptrunner/controller/PostConstructController.java
@@ -1,17 +1,26 @@
 package com.limmerlaboratories.scriptrunner.controller;
 
 import com.limmerlaboratories.scriptrunner.service.ConfigurationService;
+import com.limmerlaboratories.scriptrunner.service.UpdateService;
 import jakarta.annotation.PostConstruct;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@Log4j2
 public class PostConstructController {
     @Autowired
     private ConfigurationService configurationService;
+    @Autowired
+    private UpdateService updateService;
 
     @PostConstruct
     public void init() {
         configurationService.init();
+
+        if (updateService.checkForUpdate()) {
+            log.info("Update to version " + updateService.getLatestVersion() + " is available");
+        }
     }
 }
